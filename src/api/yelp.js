ef05cd05655337d88db9079aa801ac52d90b3231
Frank <frank7300228@gmail.com>
2020-05-11 17:54:24 +0800

    Finish 9-95

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default axios.create({
-  baseURL: "https://api.yelp.com/v3/business",
+  baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
       "Bearer s-5YMcrpWpMLj7f5IT4Whq7F3nFlr7hdig9WA1GRkTcoNqtpmaGbTpOO9iL3CDK9FLa1bydJqpdafAaWDRkLXvogm_Vv2dVaj-1hJW_6pYrHZXJKzFTiAL6esue4XnYx"
