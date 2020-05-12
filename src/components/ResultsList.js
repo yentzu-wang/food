6189829625e546b2541b024c43b43f515c19fdfd
Frank <frank7300228@gmail.com>
2020-05-12 15:03:49 +0800

    Finish 9-103

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,11 +1,18 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, FlatList } from "react-native"
 
 const ResultsList = ({ title, results }) => {
   return (
     <View>
       <Text style={styles.title}>{title}</Text>
-      <Text>Results: {results.length}</Text>
+      <FlatList
+        horizontal
+        data={results}
+        keyExtractor={result => result.id}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>
+        }}
+      />
     </View>
   )
 }
