2b7762c429d488ddc11b51e62136ad6905d02265
Frank <frank7300228@gmail.com>
2020-05-13 11:33:29 +0800

    Finish 11-115

--- src/screens/ResultShowScreen.js
+++ src/screens/ResultShowScreen.js
@@ -1,9 +1,23 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { StyleSheet, Text, View } from "react-native"
+import yelp from "../api/yelp"
 
 const ResultShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null)
   const id = navigation.getParam("id")
-  console.log(id)
+
+  console.log(result)
+
+  useEffect(() => {
+    getResult(id)
+    return () => {}
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  async function getResult(id) {
+    const response = await yelp.get(`/${id}`)
+    setResult(response.data)
+  }
 
   return (
     <View>
